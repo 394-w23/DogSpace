91771482ab6aa3e7d462a1a9a28d2b9cbd229ed5
Brevan Mitchell <brevanm@gmail.com>
2023-02-18 16:12:07 -0600

    stashing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Profile } from './pages/Profile';
 import { Category } from './pages/Category';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Form from './pages/form/Form';
+import { MoodChart } from './components/MoodChart';
 
 // will need to add logic to only show form when it hasn't been filled out, in the home page
 const App = () => {
@@ -32,7 +33,8 @@ const App = () => {
   ]);
 
   return (
-    <RouterProvider router={router} />
+    // <RouterProvider router={router} />
+    <MoodChart />
   );
 };
 
