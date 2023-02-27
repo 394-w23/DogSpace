e18aaafe1ec7aa097fa0f0e0eca6e722bc127eed
Brevan Mitchell <brevanm@gmail.com>
2023-02-27 16:41:02 -0600

    Passing two unit tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,17 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
+describe('Withou crashing', () => {
+  test('Should load training tips', () => {
     render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+    expect(screen.getByText('Paw-some Training Tips')).toBeDefined();
   });
 
-  test("Counter should increment by one when clicked", async () => {
+  test('Load home pages', async () => {
     render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
+    const homeButton = screen.getByText('Home');
+    fireEvent.click(homeButton);
+    expect(await screen.getByText('no page yet')).toBeDefined();
   });
-
 });
