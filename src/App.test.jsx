5a84789b786f960f282da06d9197960f8bb4b0bd
Brevan Mitchell <brevanm@gmail.com>
2023-02-28 09:20:00 -0600

    Working on testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,39 @@
-import { describe, expect, test } from 'vitest';
+import { vi, describe, expect, it, beforeEach } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import * as fireUtils from './utils/firebase';
 
-describe('Withou crashing', () => {
-  test('Should load training tips', () => {
-    render(<App />);
-    expect(screen.getByText('Paw-some Training Tips')).toBeDefined();
+// const useAuthStateMocked = () => vi.fn();
+vi.mock('./utils/firebase', () => ({
+  useAuthState: vi.fn(),
+  ...fireUtils
+}));
+
+describe('<App />', () => {
+  describe('with logged in user', () => {
+    beforeEach(() => {
+      render(<App />);
+    });
+
+    it('should load training tips', () => {
+      expect(screen.getByText('Paw-some Training Tips')).toBeDefined();
+    });
+
+    it('load home page', async () => {
+      const homeButton = screen.getByText('Home');
+      fireEvent.click(homeButton);
+      expect(await screen.getByText('no page yet')).toBeDefined();
+    });
   });
 
-  test('Load home pages', async () => {
-    render(<App />);
-    const homeButton = screen.getByText('Home');
-    fireEvent.click(homeButton);
-    expect(await screen.getByText('no page yet')).toBeDefined();
+  describe('without logged in user', () => {
+    beforeEach(() => {
+      useAuthStateMocked.mockImplementation(() => [null]);
+      render(<App />);
+    });
+
+    it('displays sign in page', () => {
+      expect(screen.getByText('ZENDOG')).toBeDefined();
+    });
   });
 });
