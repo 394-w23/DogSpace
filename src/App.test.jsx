3c99ec8c1d5b103ea33d35573bccac40e409c297
Brevan Mitchell <brevanm@gmail.com>
2023-02-28 18:13:14 -0600

    Testing Assignment Done

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,22 +1,49 @@
 import { vi, describe, expect, it, beforeEach } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import * as fireUtils from './utils/firebase';
+import { useAuthState, useProfile } from './utils/firebase';
 
-// const useAuthStateMocked = () => vi.fn();
-vi.mock('./utils/firebase', () => ({
-  useAuthState: vi.fn(),
-  ...fireUtils
-}));
+vi.mock('./utils/firebase', async () => {
+  const original = await vi.importActual('./utils/firebase');
+  return {
+    ...original,
+    useAuthState: vi.fn(),
+    useProfile: vi.fn()
+  };
+});
 
 describe('<App />', () => {
+  const user = { email: 'user@northwetern.edu' };
+  const profile = {
+    age: '0',
+    email: 'user@northwestern.edu',
+    gender: '',
+    name: 'Test User'
+  };
+  const dogProfile = {
+    age: '0',
+    birthday: '2023-01-01',
+    breed: 'breed',
+    email: 'user@northwestern.edu',
+    gender: '',
+    'health issues': ['barking'],
+    name: 'Test Dog'
+  };
+
   describe('with logged in user', () => {
     beforeEach(() => {
+      useAuthState.mockReturnValue([user]);
+      useProfile.mockReturnValueOnce([profile]);
+      useProfile.mockReturnValueOnce([dogProfile]);
       render(<App />);
     });
 
     it('should load training tips', () => {
       expect(screen.getByText('Paw-some Training Tips')).toBeDefined();
+    });
+
+    it('display dog name from dog profile', () => {
+      expect(screen.getByText('Test Dog')).toBeDefined();
     });
 
     it('load home page', async () => {
@@ -28,7 +55,8 @@ describe('<App />', () => {
 
   describe('without logged in user', () => {
     beforeEach(() => {
-      useAuthStateMocked.mockImplementation(() => [null]);
+      useAuthState.mockReturnValue([null]);
+      useProfile.mockReturnValue([null]);
       render(<App />);
     });
 
