dc81146cfa04322737b9c8d9429d46584569a8cc
Brevan Mitchell <brevanm@gmail.com>
2023-03-11 00:36:22 -0600

    Added coverage

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    }
   }
 });
